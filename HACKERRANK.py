6c0ca694d6e7aafb670a4627a29b40f60f3ee23f
Aishie <45102541+aishiemaddimsetty@users.noreply.github.com>
2020-02-19 11:37:05 -0600

    Add files via upload

---
+++ HACKERRANK.py
@@ -0,0 +1,14 @@
+Scores = [2,3,6,6,5]
+ScoresSet = set(Scores)
+ScoresSet = ScoresSet.remove(max(ScoresSet))
+print(max(ScoresSet))
+
+#def RunnerUp(array):
+    #maxScore = max(array)
+    #newArray = array.remove(maxScore) 
+    #print(newArray[-1])
+
+#RunnerUp(Scores)
+
+#sorted(Scores)
+#Scores.remove(max(Scores))
